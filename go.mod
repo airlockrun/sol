c060f880c66e5aa03a955bdda11d4a10226cf036
Cyber Teaborg <cyberteaborg@gmail.com>
2026-06-14 18:09:24 +0300

    deps: require goai v0.1.5-rc.1; bump to v0.1.8-rc.1
    Picks up the openaicompat dangling tool_call repair so sol's provider
    path (and anything embedding sol) gets the DeepSeek 400 fix.

--- go.mod
+++ go.mod
@@ -2,6 +2,6 @@ module github.com/airlockrun/sol
 
 go 1.26.0
 
-require github.com/airlockrun/goai v0.1.4
+require github.com/airlockrun/goai v0.1.5-rc.1
 
 require golang.org/x/net v0.52.0
--- version.go
+++ version.go
@@ -3,4 +3,4 @@ package sol
 // Version is the sol library version. Pre-commit gate enforces
 // Version >= latest git tag in this repo and (under RELEASE_TAG)
 // equality with the tag being created.
-const Version = "0.1.7"
+const Version = "0.1.8-rc.1"
