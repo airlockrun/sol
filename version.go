ef21dfcebc67ca916c85a4b38a087e48441eb7c1
Cyber Teaborg <cyberteaborg@gmail.com>
2026-07-27 22:10:29 +0300

    release: sol v0.1.9

--- version.go
+++ version.go
@@ -2,4 +2,4 @@ package sol
 
 // Version is the sol library version. It is strictly greater than the latest
 // git tag except while that exact version is being released.
-const Version = "0.1.9-alpha.1"
+const Version = "0.1.9"
